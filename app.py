c927c565d855cab2b5b2c2ebb62a2ffb9c0c26b9
Reeetesh <riteshsahu1560@gmail.com>
2022-02-14 00:35:52 +0530

    commit contrast and blur

--- app.py
+++ app.py
@@ -4,70 +4,102 @@ import imp
 from click import edit
 import cv2
 import os
-import wget
 
 from numpy import imag
 
-from edit import brightness
-from flask import Flask, redirect, render_template, url_for, request,Response
+from edit import blur, brightness, contrast, sharp
+from flask import Flask, redirect,flash, render_template, url_for, request, Response
 from wtforms import Form
 from werkzeug.utils import secure_filename
 
 
 UPLOAD_FOLDER = 'static/uploads/'
 
-app = Flask(__name__,static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
+
 @app.route('/')
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
 
-edit_img=''
-@app.route('/',methods = ["GET","POST"])
+
+edit_img = ''
+edited=''
+brightness_data='0'
+contrast_data='1'
+blur_data='0'
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-  if request.method == "POST":
-    global brightness_data
-    global contrast_data
-    global blur_data
-    global sharpness_data
-    global edit_img
-    if request.form['upload']=='Upload' and request.form['path']!='' and  request.files['local_file'].filename=='':
-           path = request.form['path']
-           a = path.split('/')
-           previous = os.getcwd()
-           edit_img=previous+'/static/images/trash/'+a[len(a)-1]
-           if os.path.isfile(previous+'/static/images/trash/'+a[len(a)-1]):
-              return render_template('edit.html',image_filename='../static/images/trash/'+a[len(a)-1])
-           folder = previous +'/static/images/trash/'
-           os.chdir(folder)
-           image_filename = wget.download(path)
-           os.chdir(previous)
-           return render_template('index.html',image_filename='../static/images/trash/'+image_filename)  
-    elif request.form['upload']=='Upload' and request.form['path']=='' and  request.files['local_file'].filename!='':
+    if request.method == "POST":
+        global brightness_data
+        global contrast_data
+        global blur_data
+        global sharpness_data
+        global edit_img
+        global edited
+        if request.form['upload'] == 'Upload' and request.form['path'] != '' and request.files['local_file'].filename == '':
+            path = request.form['path']
+            a = path.split('/')
+            previous = os.getcwd()
+            edit_img = previous+'/static/images/trash/'+a[len(a)-1]
+            if os.path.isfile(previous+'/static/images/trash/'+a[len(a)-1]):
+                return render_template('edit.html', image_filename='../static/images/trash/'+a[len(a)-1],brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+            folder = previous + '/static/images/trash/'
+            os.chdir(folder)
+            image_filename = wget.download(path)
+            os.chdir(previous)
+            return render_template('index.html', image_filename='../static/images/trash/'+image_filename,brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+        elif request.form['upload'] == 'Upload' and request.form['path'] == '' and request.files['local_file'].filename != '':
             f = request.files['local_file']
             previous = os.getcwd()
-            edit_img=previous+'/static/images/trash/'+f.filename
-            folder = previous +'/static/images/trash/'
+            edit_img = previous+'/static/images/trash/'+f.filename
+            folder = previous + '/static/images/trash/'
             os.chdir(folder)
             f.save(secure_filename(f.filename))
             os.chdir(previous)
-            return render_template('index.html',image_filename='../static/images/trash/'+f.filename)       
-    elif request.form['upload']=='submit':
-      brightness_data = request.form['brightness']
-      contrast_data = request.form['contrast']
-      blur_data = request.form['blur']
-      sharpness_data = request.form['sharpness']
-      if brightness_data:
-        brightness(edit_img,int(brightness_data))
-        
+            return render_template('index.html', image_filename='../static/images/trash/'+f.filename,brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+        elif request.form['upload'] == 'submit' and edit_img!='':
+            brightness_data = request.form['brightness']
+            contrast_data = request.form['contrast']
+            blur_data = request.form['blur']
+            sharpness_data = request.form['sharpness']
+            previous = os.getcwd()
+            folder = previous + '/static/images/trash/'
+            if edit_img.count('jpeg')>0 or edit_img.count("jpg")>0:
+              edited=folder+'edited.jpg'
+            elif edit_img.count('png')>0:
+                edited=folder+'edited.png'
+            os.chdir(folder)
+
+            if brightness_data:
+              brightness(edit_img,int(brightness_data))
+            if contrast_data:
+                contrast(edited, float(contrast_data))
+            if blur_data:
+                blur(edited,int(blur_data))
+            if sharpness_data:
+                sharp(edited,sharpness_data)
+            os.chdir(previous)
+
+            if edit_img.count('jpeg')>0 or edit_img.count("jpg")>0:
+                return render_template('index.html',image_filename='../static/images/trash/edited.jpg',brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+            elif edit_img.count('png')>0:
+                return render_template('index.html',image_filename='../static/images/trash/edited.jpg',brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+            else:
+               flash("Oops Something went wrong !")
+               return render_template('index.html',brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+        else:
+            edit_img=''
+            edited=''
+            brightness_data='0'
+            contrast_data='1' 
+            flash("Oops Something went wrong !")     
+            return render_template('index.html',brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
+    return render_template('index.html', image_filename='../static/images/edited.jpg',brightness_data=brightness_data,contrast_data=contrast_data,blur_data=blur_data)
 
 
-  return render_template('index.html',image_filename='../static/images/edited.jpg')
-  
 if __name__ == "__main__":
-  app.run(debug=True)
-
-
-
+    app.run(debug=True)
--- edit.py
+++ edit.py
@@ -1,23 +1,56 @@
+
 import numpy as np
 import cv2
 import os
 
-directory1 = r'C:\Users\rites\OneDrive\Desktop\Image-Editor\static\images'
-directory=r'C:\Users\rites\OneDrive\Desktop\Image-Editor'
+# directory1 = r'C:\Users\rites\OneDrive\Desktop\Image-Editor\static\images\trash'
+# directory=r'C:\Users\rites\OneDrive\Desktop\Image-Editor'
 
 
 def apply(path,edit_img,name="edited"):
    if path.count("jpg")>0 or path.count("jpeg")>0:
-      os.chdir(directory1)
+      #os.chdir(directory1)
       cv2.imwrite(name+'.jpg',edit_img)
    elif path.count("png")>0:
-      os.chdir(directory1)
+     # os.chdir(directory1)
       cv2.imwrite(name+'.png',edit_img)
-   os.chdir(directory)
+   #os.chdir(directory)
 
 def brightness(path,value):
     image=cv2.imread(path)
     zero=np.zeros(image.shape,image.dtype)
     bright_img = cv2.addWeighted(image,1,zero,0,value)
+    # cv2.imshow("test",bright_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     apply(path,bright_img)
    
+def contrast(path,value):
+    image=cv2.imread(path)
+    zero=np.zeros(image.shape,image.dtype)
+    con = cv2.addWeighted(image,value,zero,0,0)
+    # cv2.imshow("test",con)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows() 
+    apply(path,con)
+
+
+def sharp(path,value):
+    image=cv2.imread(path)
+    kernel = np.array([[0,-1,0], 
+                       [-1,5,-1],                     #min
+                       [0,-1,0]])
+    kernel1 =np.array([[-1,-1,-1], 
+                [-1, 9,-1],                           #max
+                [-1,-1,-1]])
+    if value=='max':
+        sharpened = cv2.filter2D(image, -1, kernel1)
+    else:
+        sharpened = cv2.filter2D(image, -1, kernel)
+    apply(path,sharpened)
+    
+
+def blur(path,value):
+    image = cv2.imread(path) 
+    blur = cv2.blur(image,(value,value)) 
+    apply(path,blur)
