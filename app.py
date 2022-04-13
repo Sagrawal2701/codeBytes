f326362612b79883242324b8dffde6fda449d7e2
Sagrawal2701 <84444366+Sagrawal2701@users.noreply.github.com>
2022-04-14 03:26:07 +0530

    deploy8

--- app.py
+++ app.py
@@ -309,5 +309,4 @@ def download_file2():
 
 ###############################################################################################main function
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
