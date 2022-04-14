58a3a0cd38528092be074ead50c547790f8ccc68
Reeetesh <riteshsahu1560@gmail.com>
2022-04-14 14:41:47 +0530

    final

--- app.py
+++ app.py
@@ -310,4 +310,4 @@ def download_file2():
 
 ###############################################################################################main function
 if __name__ == '__main__':
-	app.run(debug=False,port=8080)
+	app.run()
